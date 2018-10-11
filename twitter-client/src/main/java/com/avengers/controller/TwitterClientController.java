86af3818810a5c014c79391192231fb6419f9d34
sumanthravipatirr <sumanthravipatirr@gmail.com>
2018-10-10 17:14:12 -0700

    Junit test cases commit

--- twitter-client/src/main/java/com/avengers/controller/TwitterClientController.java
+++ twitter-client/src/main/java/com/avengers/controller/TwitterClientController.java
@@ -3,7 +3,6 @@ package com.avengers.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.twitter.api.CursoredList;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.web.bind.annotation.PathVariable;
---
+++ twitter-client/src/test/java/com/avengers/test/controller/TwitterControllerTest.java
@@ -0,0 +1,82 @@
+package com.avengers.test.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+public class TwitterControllerTest extends AbstractClass {
+	
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+	
+	@Test
+	public void testHomelineTweets() throws Exception {
+		String uri = "/api.twitter.com/1.1/statuses/home_timeline";
+		
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int statusCode = mvcResult.getResponse().getStatus();
+		assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	public void testMentionsTimelineTweets() throws Exception {
+		String uri = "/api.twitter.com/1.1/statuses/mentions_timeline";
+		
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int statusCode = mvcResult.getResponse().getStatus();
+		assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	public void testUserTimelineTweets() throws Exception {
+		String uri = "/api.twitter.com/1.1/statuses/user_timeline";
+		
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int statusCode = mvcResult.getResponse().getStatus();
+		assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	public void testReTweetsOfMe() throws Exception {
+		String uri = "/api.twitter.com/1.1/statuses/retweets_of_me";
+		
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int statusCode = mvcResult.getResponse().getStatus();
+		assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	public void testSearchTweets() throws Exception {
+		String uri = "/api.twitter.com/1.1/search/tweets/cricket";
+		
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int statusCode = mvcResult.getResponse().getStatus();
+		assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	public void testSearchTweetBasedOnId() throws Exception {
+		String uri = "/api.twitter.com/1.1/statuses/show/sharukh";
+		
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int statusCode = mvcResult.getResponse().getStatus();
+		assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	public void testUserSearchTweets() throws Exception {
+		String uri = "/api.twitter.com/1.1/users/search/sumanth";
+		
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)).andReturn();
+		int statusCode = mvcResult.getResponse().getStatus();
+		assertEquals(statusCode, 200);
+	}
+	
+
+}
